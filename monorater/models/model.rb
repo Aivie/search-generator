9699e3ca6bb4ab0beb5e2298171ed4f9fe8580d8
Jackie He <Jackie.He8@gmail.com>
2016-07-22 14:21:00 +0000

    First commit

--- monorater/models/model.rb
+++ monorater/models/model.rb
@@ -14,8 +14,8 @@ def check_input(user_input)
     
         if user_input == ""
         
-            input="random"
-            url = "http://api.giphy.com/v1/gifs/search?q=#{input}&api_key=dc6zaTOxFJmzC"
+            input=["Sad","Bad","happy","love","why","murder","mystery","holy","pokemon","phantom","ghost", "comedy", "romance", "action", "drama", "mecha", "disney", "cute", "funny", "vine", "loop"]
+            url = "http://api.giphy.com/v1/gifs/search?q=#{input.sample}&api_key=dc6zaTOxFJmzC"
             uri = URI(url)
             response = Net::HTTP.get(uri)
             data = JSON.parse(response)
@@ -101,31 +101,55 @@ end
 #yummly
 
 #e54e9cfa05d58b64bab7da2301f05c0b&
-require 'pry'
+# require 'pry'
 
 
 
 
 def food_search
     
-    Yummly.configure do |config|
-        config.app_id = "4e9f2aa0&"
-        config.app_key = "e54e9cfa05d58b64bab7da2301f05c0b"
-        config.use_ssl = true # Default is false
-    end
+    user_input = "sad"
     
-    user_input = "happy"
+    url = "https://api.yummly.com/v1/api/recipes?_app_id=4e9f2aa0%26&_app_key=e54e9cfa05d58b64bab7da2301f05c0b&q=#{user_input}"
+    uri = URI(url)
+    response = Net::HTTP.get(uri)
+    data = JSON.parse(response)
+    
+    get_food = data["matches"].sample
+    food_name = get_food["recipeName"]
+    food_img = get_food["imageUrlsBySize"]["90"]
+    food_attr = get_food["attributes"]["course"].each do |i|
+        puts i
+    end
+    food_id = get_food["id"]
+    food_link = "http://www.yummly.com/recipe/#{food_id}"
+    #http://www.yummly.com/recipe/#{id} to link to food
+    puts food_link
+    puts food_name
+    puts food_img
+    
+    # Yummly.configure do |config|
+    #     config.app_id = "4e9f2aa0&"
+    #     config.app_key = "e54e9cfa05d58b64bab7da2301f05c0b"
+    #     config.use_ssl = true # Default is false
+    # end
+    
+    
 
-    result = Yummly.search(user_input)
-    find_food = result["matches"]
-    random_food = find_food.sample
-    puts random_food["recipeName"]
+    # result = Yummly.search(user_input)
+    
+    # #The food's name 
+    # get_names = result.collect { |recipe| recipe.name }
+    # name = get_names.sample
+    # puts name
+    
+    #An image of the food collected
     
 end
 
 food_search
 
-binding.pry
+# binding.pry
 
 
 
