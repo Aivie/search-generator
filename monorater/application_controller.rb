c26af59dcc6bddf492ece79f2cb622bb9d473769
Jackie He <Jackie.He8@gmail.com>
2016-07-22 15:52:53 +0000

    Added Yummly

--- monorater/application_controller.rb
+++ monorater/application_controller.rb
@@ -12,7 +12,13 @@ class ApplicationController < Sinatra::Base
   post '/result' do 
     @gif_result = check_input(params[:user_input])
     @movie_result = find_movie(params[:user_input]) 
+    @food_result = food_search(params[:user_input])
     erb :result, :layout => :base
   end
+
+
+# get '/www.imdb.com/title/#{@movie_id}/' do
+#     "Hello World"
+#   end
 
 end
--- monorater/models/model.rb
+++ monorater/models/model.rb
@@ -8,13 +8,9 @@ require 'yummly'
 #Gif Searcher
 def check_input(user_input)
     
-#     used = 0
-    
-#     while used <= 8 do
-    
         if user_input == ""
         
-            input=["Sad","Bad","happy","love","why","murder","mystery","holy","pokemon","phantom","ghost", "comedy", "romance", "action", "drama", "mecha", "disney", "cute", "funny", "vine", "loop"]
+            input=["Sad","Bad","happy","love","why", "anime","murder","mystery","holy","pokemon","phantom","ghost", "comedy", "romance", "action", "drama", "mecha", "disney", "cute", "funny", "vine", "loop"]
             url = "http://api.giphy.com/v1/gifs/search?q=#{input.sample}&api_key=dc6zaTOxFJmzC"
             uri = URI(url)
             response = Net::HTTP.get(uri)
@@ -22,8 +18,7 @@ def check_input(user_input)
             random_gif = data["data"].sample
             image = random_gif["images"]
             url = image["original"]["url"]
-            # used = used + 1
-        
+
         else
             def search_gif(user_input)
     
@@ -34,7 +29,6 @@ def check_input(user_input)
                 random_gif = data["data"].sample
                 image = random_gif["images"]
                 url = image["original"]["url"]
-                # used = used + 1
 
             end
     
@@ -42,19 +36,15 @@ def check_input(user_input)
 
         end
     
-    # end
-    
 end
 
 
-
-
-
+#Movie Search
 def find_movie(user_input)
     
     if user_input == ""
         
-        input = ["Sad","Bad","happy","love","why","murder","mystery","holy","pokemon","phantom","ghost", "comedy", "romance", "action", "drama", "mecha", "disney"]
+        input = ["Sad","Bad","happy","love","why","murder", "anime","mystery","holy","pokemon","phantom","ghost", "comedy", "romance", "action", "drama", "mecha", "disney"]
         search = OMDB.search(input.sample)
         movie = search.sample
         @movie_title = movie[:title]
@@ -72,82 +62,63 @@ def find_movie(user_input)
         @movie_img = movie[:poster]
         
     end
-    #to link to the movie use www.imdb.com/title/#{id}/ and fill out neccessary perams
+    
 end
 
+
+#Song search
 def find_song(user_input)
+    
 end
 
 
-
-
-
-# def gif_repeat(check_input(user_input))
-#     used_gifs = []
-#     gifs = ''
-#     gifs = check_input(user_input) 
-#     used_gifs << gifs
-#     if check_input(user_input) == used_gifs.sample
+def food_search(user_input)
+    
+    if user_input == ""
         
-#         check_input(user_input)
+        input = ["Disney", "Pokemon", "Happy", "Sad", "Angry", "Cool", "Easy", "No-Bake", "Macaroons", "Brownies", "Cookies", "Cake", "Cupcake", "Chicken", "Dessert", "Lunch", "Breakfast", "Dinner", "Brunch", "Sandwich", "Fries", "Ice-Cream"]
         
-#     else
+        url = "https://api.yummly.com/v1/api/recipes?_app_id=4e9f2aa0%26&_app_key=e54e9cfa05d58b64bab7da2301f05c0b&q=#{input.sample}"
+        uri = URI(url)
+        response = Net::HTTP.get(uri)
+        data = JSON.parse(response)
+    
+        get_food = data["matches"].sample
+        @food_name = get_food["recipeName"]
+        @food_img = get_food["imageUrlsBySize"]["90"]
+        @food_attr = get_food["attributes"]["course"].each do |i|
+            puts i
+        end
+        food_id = get_food["id"]
+        @food_link = "http://www.yummly.com/recipe/#{food_id}"
+        puts @food_link
+        puts @food_name
+        puts @food_img
         
-#     end
+    else
+        
+        url = "https://api.yummly.com/v1/api/recipes?_app_id=4e9f2aa0%26&_app_key=e54e9cfa05d58b64bab7da2301f05c0b&q=#{user_input}"
+        uri = URI(url)
+        response = Net::HTTP.get(uri)
+        data = JSON.parse(response)
     
-    
-# end
-
-#yummly
-
-#e54e9cfa05d58b64bab7da2301f05c0b&
-# require 'pry'
-
-
-
-
-def food_search
-    
-    user_input = "sad"
-    
-    url = "https://api.yummly.com/v1/api/recipes?_app_id=4e9f2aa0%26&_app_key=e54e9cfa05d58b64bab7da2301f05c0b&q=#{user_input}"
-    uri = URI(url)
-    response = Net::HTTP.get(uri)
-    data = JSON.parse(response)
-    
-    get_food = data["matches"].sample
-    food_name = get_food["recipeName"]
-    food_img = get_food["imageUrlsBySize"]["90"]
-    food_attr = get_food["attributes"]["course"].each do |i|
-        puts i
+        get_food = data["matches"].sample
+        @food_name = get_food["recipeName"]
+        @food_img = get_food["imageUrlsBySize"]["90"]
+        @food_attr = get_food["attributes"]["course"].each do |i|
+            puts i
+        end
+        food_id = get_food["id"]
+        @food_link = "http://www.yummly.com/recipe/#{food_id}"
+        puts @food_link
+        puts @food_name
+        puts @food_img
+        
     end
-    food_id = get_food["id"]
-    food_link = "http://www.yummly.com/recipe/#{food_id}"
-    #http://www.yummly.com/recipe/#{id} to link to food
-    puts food_link
-    puts food_name
-    puts food_img
-    
-    # Yummly.configure do |config|
-    #     config.app_id = "4e9f2aa0&"
-    #     config.app_key = "e54e9cfa05d58b64bab7da2301f05c0b"
-    #     config.use_ssl = true # Default is false
-    # end
-    
-    
-
-    # result = Yummly.search(user_input)
-    
-    # #The food's name 
-    # get_names = result.collect { |recipe| recipe.name }
-    # name = get_names.sample
-    # puts name
-    
-    #An image of the food collected
     
 end
 
-food_search
+# food_search(user_input)
 
 # binding.pry
 
